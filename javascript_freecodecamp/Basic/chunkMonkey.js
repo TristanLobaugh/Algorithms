e22e1d94dc6b5118476241f3c6b07e67b33dfb5b
Tristan Lobaugh <tlobaugh@gmail.com>
2021-02-14 12:09:11 -0500

    new algo

---
+++ javascript_freecodecamp/Basic/chunkMonkey.js
@@ -0,0 +1,19 @@
+// function chunkArrayInGroups(arr, size) {
+//   const newArray = [];
+//   while (arr.length > 0) {
+//     const tempArray = [];
+//     for (let i = 0; i < size; i++) {
+//       tempArray[i] = arr.shift();
+//     }
+//     newArray.push(tempArray);
+//   }
+//   return newArray;
+// }
+
+function chunkArrayInGroups(arr, size) {
+  var newArr = [];
+  while (arr.length) {
+    newArr.push(arr.splice(0, size));
+  }
+  return newArr;
+}
